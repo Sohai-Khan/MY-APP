9bd2b2cf3b26998d0e3c5e30e13bd7e87dc42f1e
Sohai-Khan <sohail.khan@techletspk.com>
2024-10-10 12:21:44 +0500

    My Portfolio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component'; // Make sure to create this component
+import { AboutComponent } from './about/about.component'; // Create this component
+import { ServicesComponent } from './services/services.component'; // Create this component
+import { ContactComponent } from './contact/contact.component'; // Create this component
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+  // Add other routes as needed
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
