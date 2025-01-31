cbb7497b532c867d7b2163e15ba6df13397ee5c4
SOHAIL KHAN <sohail.kashmir.34@gmail.com>
2025-01-31 13:08:36 +0500

    First commit

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,18 +10,19 @@ export class ContactComponent {
   contact = {
     name: '',
     email: '',
+    password: '',
     message: ''
   };
-  
+
   submitted = false;
 
   onSubmit() {
     // Handle form submission logic here, such as sending the data to a server.
     console.log('Contact form submitted:', this.contact);
     this.submitted = true;
-    
+
     // Reset the form (optional)
-    this.contact = { name: '', email: '', message: '' };
+    this.contact = { name: '', email: '', password: '', message: '' };
   }
 }
 
